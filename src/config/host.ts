a11dfcdd6288fbf33600efe56194daf7f7ff39a8
bangbang6 <577174667@qq.com>
2025-06-02 18:22:41 +0800

    feat: init

--- src/config/host.ts
+++ src/config/host.ts
@@ -8,12 +8,12 @@ import { isDevNoMock, isPrd, isPrdDev } from '../utils/env'
 // API host
 let APIHost = '' // 默认为本地运行 mock
 if (isDevNoMock) APIHost = 'http://localhost:3003' // 本地，不用 mock
-if (isPrdDev) APIHost = 'http://82.157.160.85:8090' // 测试机
+if (isPrdDev) APIHost = 'http://82.157.160.85:8084' // 测试机
 if (isPrd) APIHost = '' // 线上环境，用当前域名即可
 
 // h5 host
 let H5Host = 'http://82.157.160.85:8082'
-if (isPrd) H5Host = 'http://82.157.160.85:8090'
+if (isPrd) H5Host = 'http://82.157.160.85:8082'
 
 // 统计接口的 host
 let StatHost = 'http://82.157.160.85:8090'
